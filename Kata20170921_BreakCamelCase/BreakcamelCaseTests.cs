04c2180c0966474214be38b76978ebb496ad3158
cash <cash@cashwu.com>
2017-09-21 18:25:49 +0800

    init

--- Kata20170921_BreakCamelCase/BreakcamelCaseTests.cs
+++ Kata20170921_BreakCamelCase/BreakcamelCaseTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170921_BreakCamelCase
 {
     [TestClass]
-    public class UnitTest1
+    public class BreakcamelCaseTests
     {
         [TestMethod]
         public void TestMethod1()
