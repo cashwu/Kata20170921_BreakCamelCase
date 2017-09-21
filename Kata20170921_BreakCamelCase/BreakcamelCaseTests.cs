3b43df25928930eec67f841d4a697f45d41bdf2c
cash <cash@cashwu.com>
2017-09-21 18:29:04 +0800

    input abc test case

--- Kata20170921_BreakCamelCase/BreakcamelCaseTests.cs
+++ Kata20170921_BreakCamelCase/BreakcamelCaseTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170921_BreakCamelCase
     public class BreakcamelCaseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_abc_should_return_abc()
         {
+            BreakCamelCaseShouldBe("abc", "abc");
+        }
+
+        private static void BreakCamelCaseShouldBe(string expected, string str)
+        {
+            var kata = new Kata();
+            var actual = kata.BreakCamelCase(str);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string BreakCamelCase(string str)
+        {
+            return str;
         }
     }
 }
