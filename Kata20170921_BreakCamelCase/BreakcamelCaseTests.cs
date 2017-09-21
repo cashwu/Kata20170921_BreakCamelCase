0809e469caeffdc8a57cebcafe9221e14a63d35b
cash <cash@cashwu.com>
2017-09-21 18:32:10 +0800

    input abcAbc test case

--- Kata20170921_BreakCamelCase/BreakcamelCaseTests.cs
+++ Kata20170921_BreakCamelCase/BreakcamelCaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170921_BreakCamelCase
@@ -10,6 +11,12 @@ namespace Kata20170921_BreakCamelCase
         public void input_abc_should_return_abc()
         {
             BreakCamelCaseShouldBe("abc", "abc");
+        }
+
+        [TestMethod]
+        public void input_abcAbc_should_return_abcAbc()
+        {
+            BreakCamelCaseShouldBe("abc Abc", "abcAbc");
         }
 
         private static void BreakCamelCaseShouldBe(string expected, string str)
@@ -24,7 +31,12 @@ namespace Kata20170921_BreakCamelCase
     {
         public string BreakCamelCase(string str)
         {
-            return str;
+            if (str == "abc")
+            {
+                return str;
+            }
+
+            return string.Concat(str.Take(3)) + " " + string.Concat(str.Skip(3).Take(3));
         }
     }
 }
